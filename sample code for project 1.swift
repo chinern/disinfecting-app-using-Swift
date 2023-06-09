293a4b6e77949439aa66c2214adfa1a7cf27b09e
Ng Chin Ern <chinern123@gmail.com>
2023-06-09 09:41:04 +0800

    Update sample code for project 1.swift

--- sample code for project 1.swift
+++ sample code for project 1.swift
@@ -8,7 +8,6 @@ let lastRunDate = Date1 {
 //implement docker, maybe to include daemon
 
 func userinterface1() {
-    // Create the user interface and run the app
     let contentView = ContentView()
     SwiftUI.App
         .main()
@@ -43,42 +42,6 @@ let Date1 {
         minute: 00)
 }
 
-// Define your main function or entry point
-func main() {
-    // Check if cooldown has expired
-    if let lastRunDate = UserDefaults.standard.object(forKey: "LastRunDate") as Date? {
-        let currentDate = Date()
-        let calendar = Calendar.current
-
-        // Calculate the time difference between the current date and the last run date
-        if let cooldownDate = calendar.date(byAdding: .hour, value: 24, to: lastRunDate) {
-            let comparisonResult = calendar.compare(currentDate, to: cooldownDate, toGranularity: .day)
-
-            // Check if cooldown has expired
-            if comparisonResult == .orderedDescending || comparisonResult == .orderedSame {
-                // Cooldown has expired, perform action
-                performAction()
-
-                // Update the last run date to the current date
-                UserDefaults.standard.set(currentDate, forKey: "LastRunDate")
-            } else {
-                // Cooldown has not expired yet
-                print("Cooldown still active. Wait for \(calendar.dateComponents([.hour, .minute, .second], from: currentDate, to: cooldownDate))")
-            }
-        }
-    } else {
-        // First run, perform action
-        performAction()
-
-        // Save the current date as the last run date
-        UserDefaults.standard.set(Date(), forKey: "LastRunDate")
-    }
-}
-
-func performAction() {
-    // Your code to perform the desired action here
-    print("Action performed")
-}
 
 // Call the main function to start the program
 main()
